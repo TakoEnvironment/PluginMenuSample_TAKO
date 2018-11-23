15808afcb8d7deacd8c5503b40ab67daea7afd73
TakoEnvironment <45265257+TakoEnvironment@users.noreply.github.com>
2018-11-24 07:54:34 +0900

    Update create_menu.c

--- create_menu.c
+++ create_menu.c
@@ -5,15 +5,15 @@
 void	my_menus(void)
 {
 // この部分にはタイトルが同じ場合でも違う場合でも共通した処理を書きます。(任意)
-	if(get_tid_low() == TITLE)
-  {
-  // この中にゲームタイトルが同じ場合の処理を書きます。( { から } まで)
-  // 例：new_entry("Cheat",code_1);
-	}
-	else
-  { 
-  // この中にゲームタイトルが違う場合の処理を書きます。( { から } まで)
-  // 例：new_unselectable_entry("Your title is wrong!");
-	}
+if(get_tid_low() == TITLE)
+{
+// この中にゲームタイトルが同じ場合の処理を書きます。( { から } まで)
+// 例：new_entry("Cheat",code_1);
+}
+else
+{ 
+// この中にゲームタイトルが違う場合の処理を書きます。( { から } まで)
+// 例：new_unselectable_entry("Your title is wrong!");
+}
 // この部分にもタイトルが同じ場合でも違う場合でも共通した処理を書きます。(任意)
 }
